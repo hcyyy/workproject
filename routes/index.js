045d9ec7674b160adecb3f6bf0342990c97d7c9c
hcy <18260994660@163.com>
2018-11-24 18:11:28 +0800

    基础样式,极其简陋

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,36 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/wind.html', function (req, res) {
+  res.render('wind');
+});
+
+router.get('/fire.html', function (req, res) {
+  res.render('fire');
+});
+
+router.get('/water.html', function (req, res) {
+  res.render('water');
+});
+
+router.get('/soil.html', function (req, res) {
+  res.render('soil');
+});
+
+router.get('/light.html', function (req, res) {
+  res.render('light');
+});
+
+router.get('/dark.html', function (req, res) {
+  res.render('dark');
+});
+
+router.get('/superman.html', function (req, res) {
+  res.render('superman');
 });
 
 module.exports = router;
